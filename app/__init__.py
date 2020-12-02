15681e0ade3de098a6526faf5fc75a68114a696f
Mestro0 <75369602+Mestro0@users.noreply.github.com>
2020-12-02 14:04:27 +0200

    update
    1111

--- app/__init__.py
+++ app/__init__.py
@@ -22,3 +22,11 @@ async def on_message(message):
         await message.channel.send('Привет ,я бот. Пока я ничего не умею ,но я буду учится ,что бы стать лучше ')
     if message.content.startswith('!команды'):
         await message.channel.send('!привет\n !about\n !команды')
+    if message.content.startswith('!бот подайте стол'):
+        await message.channel.send('┬─┬ノ( º _ ºノ) пожалуйста')
+     if message.content.startswith('!бот переверни стол'):
+        await message.channel.send('( ╯°□°)╯ ┻━━┻')
+     if message.content.startswith('!спасибо бот'):
+        await message.channel.send('ат души, братан')
+     if message.content.startswith('!Бот дай прогноз'):
+        await message.channel.send('Для успеха нужно построить Зиккурат')
