e9797c39e38e0a01166916b39e7c7a9a86883c1d
Vladislav <mitisit89@gmail.com>
2020-12-21 07:52:54 +0300

    base commands

--- commands/command.js
+++ commands/command.js
@@ -1,12 +1,18 @@
+const { prefix } = require("../config.json");
 
+module.exports = (bot, aliases, callback) => {
+  if (typeof aliases === "string") {
+    aliases = [aliases];
+  }
+  bot.on("message", (message) => {
+    const { content } = message;
+    for (const alias of aliases) {
+      const cmd = `${prefix}${alias}`;
 
-
-
-
-
-
-module.exports=[
-    {
-        
+      if (content.startsWith(`${cmd}`) || content === cmd) {
+        console.log(`running the command ${cmd}`);
+        callback(message);
+      }
     }
-]
+  });
+};
--- index.js
+++ index.js
@@ -1,19 +1,41 @@
-const Discord = require('discord.js');
-const bot=new Discord.Client()
-const commands = require('./commands/command.js')
-const fs = require('fs');
-const config =require('./config.json')
+const { Client } = require("discord.js");
+const bot = new Client();
+const command = require("./commands/command.js");
+const fs = require("fs");
+const { token } = require("./config.json");
 
-const TOKEN = config.token;
-let prefix=config.prefix
-
-
-bot.on('ready',()=>{
-    console.log(`ready ${bot.user.tag} to working`);
-})
-bot.on('message',(msg)=>{
-    if(msg.content==='ping'){
-       msg.channel.send('pong');
+bot.on("ready", () => {
+  console.log(`ready ${bot.user.tag} to working`);
+  command(bot, ["ping"], (message) => {
+    message.channel.send("pong");
+  });
+  command(bot, ["servers"], (message) => {
+    bot.guilds.cache.forEach((guild) => {
+      message.channel.send(
+        `$ У сервера  ${guild.name} всего ${guild.memberCount} участников `
+      );
+    });
+  });
+  command(bot, ["cc", "clearchannel"], (message) => {
+    if (message.member.hasPermission("ADMINISTRATOR")) {
+      message.channel.messages.fetch().then((result) => {
+        message.channel.bulkDelete(result);
+      });
     }
-})
-bot.login(TOKEN)
+  });
+  command(bot, "status", (message) => {
+    const content = message.content.replace("!status", "");
+    bot.user.setPresence({
+      activity: {
+        name: content,
+        type: 0,
+      },
+    });
+  });
+  command(bot,"карп",(message)=>{
+    message.channel.send(`Карп жив и велик как никогда ${message.author},его никто убивал , это все враки`)
+  })
+
+});
+
+bot.login(token).catch((e) => console.error(e));
